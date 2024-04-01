97f55a8b73a1c2995aa890094ef326d272c3f203
vishalant23 <vishal.pawar@antllp.com>
2024-04-01 16:16:10 +0530

    remove cross error

--- server.mjs
+++ server.mjs
@@ -2,18 +2,19 @@ import express from 'express';
 import connectDB from './db.js'; // Assuming db.js exports a function
 import userRouter from './routers/userRouters.js'; // Assuming userRouters.js exports a router
 import joblistRouter from './routers/joblistRouters.js';
-
+import cors from 'cors'
 const app = express();
 
 // Apply CORS middleware before routes
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // Parse JSON request body
 app.use(express.json());
+app.use(cors())
 
 // Routes
 app.use('/api', userRouter);
